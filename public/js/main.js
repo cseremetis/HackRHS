3d66fc188147626906b69adc24e90453bce30be5
cseremetis <christianseremetis@gmail.com>
2016-03-04 22:47:01 -0500

    updated

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+/*!
+*4 March 2016
+*HackRHS
+*Created by Christian Seremetis
+*All Rights Reserved
+*/
+
+$(document).ready(function(){
+
+	//when buttons are clicked
+	$('#student').click(function(e){
+		e.preventDefault();
+		$.fn.fullpage.moveTo(4);
+	});
+
+	$('#sponsorBtn').click(function(e){
+		e.preventDefault();
+		$.fn.fullpage.moveTo(3);
+	});
+
+	function activeScroll(){
+
+		$('.section').hover(function(){
+			$('ul li').removeClass('active')
+		})
+
+		$('#index').hover(function(){
+			$('#1').addClass('active');
+		});
+
+		$('#faqs').hover(function(){
+			$('#2').addClass('active');
+		});
+
+		$('#sponsor').hover(function(){
+			$('#3').addClass('active');
+		});
+
+		$('#signup').hover(function(){
+			$('#4').addClass('active');
+		});
+
+		$('#contact').hover(function(){
+			$('#5').addClass('active');
+		});
+	}
+
+	activeScroll();
+});
+
